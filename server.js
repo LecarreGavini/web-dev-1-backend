fc658bdffa225a614e17bb50c0053f28db9c571a
Lecarre Gavini <lecarregavini@gmail.com>
2025-11-29 11:18:27 +0800

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors') // * new
+
+const app = express()
+const port = 3000
+
+app.use(cors()) // * new
+app.use(express.json()) // * new
+
+app.get('/', (request, response) => {
+	console.log('getting')
+	response.json({
+		name: 'Lecarre',
+		age: 21,
+		description: 'faculty',
+	})
+})
+
+app.post('/', (request, response) => {
+	console.log(request.body)
+	const data = request.body
+	response.json({
+		success: data.id == 1 ? true : false,
+		message:
+			data.id == 1
+				? 'Your information has been saved to the database'
+				: 'Your information has not been saved to the database',
+	})
+})
+
+app.get('/department-public-work-highways', (request, response) => {
+	console.log('getting')
+	response.json({
+		plundered: 700000000,
+		who: ['JR', 'Mark', 'AJ', 'Christian', 'Kat'],
+	})
+})
+
+app.listen(port)
