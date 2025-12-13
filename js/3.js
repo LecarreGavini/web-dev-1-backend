03e316c7c4268a39119c2e75d0779d0f3659d231
Lecarre Gavini <lecarregavini@gmail.com>
2025-12-13 12:02:04 +0800

    fix conflict

--- js/3.js
+++ js/3.js
@@ -1,13 +1,13 @@
-document.querySelectorAll('.tab').forEach((tab, index) => {
+document.querySelectorAll('.tab2').forEach((tab, index) => {
 	tab.addEventListener('click', async () => {
 		const req = await fetch(`http://127.0.0.1:3000/topics?tab=${index}`)
 		const data = await req.json()
 
-		document.getElementById('title').innerText = data.title
-		document.getElementById('label').innerText = data.label
-		document.getElementById('description').innerText = data.description
+		document.getElementById('title2').innerText = data.title
+		document.getElementById('label2').innerText = data.label
+		document.getElementById('description2').innerText = data.description
 
-		const topicsContainer = document.getElementById('topics')
+		const topicsContainer = document.getElementById('topics2')
 
 		const topicsEl = data.topics.map(topic => {
 			const element = document.createElement('li')
